79874939ce5e2b553163fcd6e11a1e67b20b0793
DesertDogSledder <desertdogsledder@gmail.com>
2017-11-13 10:53:52 -0600

    added missing exception handling for repair function

--- lib/update.py
+++ lib/update.py
@@ -29,49 +29,54 @@ def update_wca():
 
 
 def repair_wca():
-    out, err = subprocess.Popen(('git', 'init'), stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-    out = out.decode().strip()
-    err = err.decode().strip()
-    if out != '':
-        print(out)
-    if err != '':
-        print(err)
-    print()
+    try:
+        out, err = subprocess.Popen(('git', 'init'), stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+        out = out.decode().strip()
+        err = err.decode().strip()
+        if out != '':
+            print(out)
+        if err != '':
+            print(err)
+        print()
 
-    out, err = subprocess.Popen(('git', 'remote', 'add', 'origin', 'https://github.com/DesertDogSledder/wca'), stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-    out = out.decode().strip()
-    err = err.decode().strip()
-    if out != '':
-        print(out)
-    if err == 'fatal: remote origin already exists.':
-        print('Remote origin already set.')
-    else:
-        print(err)
-    print()
+        out, err = subprocess.Popen(('git', 'remote', 'add', 'origin', 'https://github.com/DesertDogSledder/wca'), stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+        out = out.decode().strip()
+        err = err.decode().strip()
+        if out != '':
+            print(out)
+        if err == 'fatal: remote origin already exists.':
+            print('Remote origin already set.')
+        else:
+            print(err)
+        print()
 
-    out, err = subprocess.Popen(('git', 'fetch', 'origin', 'master'), stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-    out = out.decode().strip()
-    err = err.decode().strip()
-    if out != '':
-        print(out)
-    if err != '':
-        print(err)
-    print()
+        out, err = subprocess.Popen(('git', 'fetch', 'origin', 'master'), stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+        out = out.decode().strip()
+        err = err.decode().strip()
+        if out != '':
+            print(out)
+        if err != '':
+            print(err)
+        print()
 
-    out, err = subprocess.Popen(('git', 'reset', '--hard', 'FETCH_HEAD'), stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-    out = out.decode().strip()
-    err = err.decode().strip()
-    if out != '':
-        print(out)
-    if err != '':
-        print(err)
-    print()
+        out, err = subprocess.Popen(('git', 'reset', '--hard', 'FETCH_HEAD'), stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+        out = out.decode().strip()
+        err = err.decode().strip()
+        if out != '':
+            print(out)
+        if err != '':
+            print(err)
+        print()
 
-    out, err = subprocess.Popen(('git', 'branch', '--set-upstream-to=origin/master', 'master'), stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-    out = out.decode().strip()
-    err = err.decode().strip()
-    if out != '':
-        print(out)
-    if err != '':
-        print(err)
-    print()
+        out, err = subprocess.Popen(('git', 'branch', '--set-upstream-to=origin/master', 'master'), stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+        out = out.decode().strip()
+        err = err.decode().strip()
+        if out != '':
+            print(out)
+        if err != '':
+            print(err)
+        print()
+    except FileNotFoundError:
+        print("\nError: Git not found. It's either not installed or not in "
+              "the PATH environment variable like requested in the guide.")
+        return
